3b8ded4b253b5b9a03069284ef0c4781d3323ce0
WireLess-00 <wire00less00@gmail.com>
2025-08-07 03:31:21 +0200

    game project

---
+++ rolegame.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/*
+Créer un jeu de rôle:
+
+- On nous demande quel rôle on veut être
+- Png qui explique le monde au début du jeu (univers)
+- il a fini de parler, un combat démarre,
+- avoir un système de combat avec des hp des deux côtés
+- voir ceux de l'ennemi et les tiens
+- mini choix de compétences
+- Fin de combat avec système d'xp
+- Dialogue pour célébrer ton combat ou bien ta défaite
+#include <stdio.h>
+*/
+
+typedef struct Roles
+{
+    int Ghost;
+    int Alien;
+    int Possessed;
+    int Medium;
+
+}Roles;
+
+
+int main(void)
+{
+    printf("Vous êtes envoyé au Japon pour combattre Pépé Turbo qui veut éliminer Mémé Turbo, seul problème si Pépé Turbo élimine Mémé Turbo elle ne nous révélera jamais ou est cacher la deuxième boule d'Okarun.\n");
+    printf("Choisissez une classe pour anéantir Pépé Turbo :\n- Ghost\n- Alien\n- Possessed\n- Medium\n");
+    
+    
+    
+    
+    
+    return 0;
+}
