1caaa541c4fe0db9262b687a73517b482e4fae2e
WireLess-00 <wire00less00@gmail.com>
2025-08-08 12:39:15 +0200

    add enum and struc ...

--- rolegame.c
+++ rolegame.c
@@ -12,6 +12,12 @@ Créer un jeu de rôle:
 - Dialogue pour célébrer ton combat ou bien ta défaite
 #include <stdio.h>
 */
+typedef enum Genres
+{
+    Male,
+    Female
+
+}Genres;
 
 typedef enum Roles
 {
@@ -22,16 +28,37 @@ typedef enum Roles
 
 }Roles;
 
+typedef enum Skills
+{
+    Skill1,
+    Skill2,
+    Skill3
+
+}Skills;
+
 typedef struct Player
 {
     unsigned char Name;
+    Genres Genres;
     Roles Roles;
+    int Hp;
+    int Attack;
+    int Defense;
+    int Xp;
+    Skills Skills;
 
 }Player;
 
 
 int main(void)
 {
+    Player Player1;
+    Player1.Hp = 100;
+    Player1.Xp = 0
+
+    // if role nanan attaque et defe == x tout les role et finir avec un else ?
+    
+
     printf("Vous êtes envoyé au Japon pour combattre Pépé Turbo qui veut éliminer Mémé Turbo, seul problème si Pépé Turbo élimine Mémé Turbo elle ne nous révélera jamais ou est cacher la deuxième boule d'Okarun.\n");
     printf("Choisissez une classe pour anéantir Pépé Turbo :\n- Ghost\n- Alien\n- Possessed\n- Medium\n");
     
