29d87f8ed1ce527df9a869445e4e52f85c1a92ac
WireLess-00 <wire00less00@gmail.com>
2025-08-14 12:12:50 +0200

    fix gender and add peudos system

--- rolegame.c
+++ rolegame.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/*
-Créer un jeu de rôle:
+#define PlayerName() printf("Votre pseudo est : %c%c%c%c%c%c%c%c\n", Player1.name.namec1, Player1.name.namec2, Player1.name.namec3, Player1.name.namec4, Player1.name.namec5, Player1.name.namec6, Player1.name.namec7, Player1.name.namec8)
 
+/*
 - On nous demande quel rôle on veut être
 - Png qui explique le monde au début du jeu (univers)
 - il a fini de parler, un combat démarre,
@@ -10,14 +10,26 @@ Créer un jeu de rôle:
 - mini choix de compétences
 - Fin de combat avec système d'xp
 - Dialogue pour célébrer ton combat ou bien ta défaite
-#include <stdio.h>
 */
+
 typedef enum Genres
 {
-    Male,
+    Male = 1,
     Female
 
 }Genres;
+
+typedef struct Name
+{
+    char namec1;
+    char namec2;
+    char namec3;
+    char namec4;
+    char namec5;
+    char namec6;
+    char namec7;
+    char namec8;
+}Name;
 
 typedef enum Roles
 {
@@ -38,14 +50,14 @@ typedef enum Skills
 
 typedef struct Player
 {
-    unsigned char Name;
-    Genres Genres;
-    Roles Roles;
-    int Hp;
-    int Attack;
-    int Defense;
-    int Xp;
-    Skills Skills;
+    Name name;
+    Genres genres;
+    Roles roles;
+    int hp;
+    int attack;
+    int defense;
+    int xp;
+    Skills skills;
 
 }Player;
 
@@ -53,14 +65,131 @@ typedef struct Player
 int main(void)
 {
     Player Player1;
-    Player1.Hp = 100;
-    Player1.Xp = 0
+    Player1.hp = 100;
+    Player1.xp = 0;
+    Player1.genres = 0;
+
+    unsigned int sure = 0;
+    unsigned int yes = 1;
+    unsigned int no = 2;
+
+    while (Player1.genres == 0) 
+    {
+        printf("Choisissez votre genre :\n\n1. Gros Male\n2.Petite Femelle\n\n");
+        scanf("%d", &Player1.genres);
+        printf("\n");
+
+        if (Player1.genres == Male)
+        {
+            printf("Vous avez dit Un Male virile ?\n\n1.Oui\n2.Non\n\n");
+        }
+        else if (Player1.genres == Female)
+        {
+            printf("Vous avez dit Une Petite femelle sans défense ?\n\n1.Oui\n2.Non\n\n");
+        }
+        scanf("%d",&sure);
+        printf("\n");
+
+        if (sure == yes)
+        {
+            if (Player1.genres == Male) 
+            {
+                printf("Vous êtes un garçon.\n");
+                printf("\n");
+            }
+            else if (Player1.genres == Female) 
+            {
+                printf("Vous êtes une fille.\n");
+                printf("\n");
+            }
+        }
+        else
+        {
+            Player1.genres = 0;
+        }
+    }
+
+    printf("Maintenant choisissez votre Pseudo caractères par caractères avec un minimum de 3 caractères et un maximum de 8 caractères.\n");
+    printf("\n");
+    printf("Ecrivez le premier caractère :\n");
+    getchar();
+    Player1.name.namec1 = getchar();
+    getchar();
+    printf("\n");
+    printf("Ecrivez le deuxième caractère :\n");
+    Player1.name.namec2 = getchar();
+    getchar();
+    printf("\n");
+    printf("Ecrivez le troisième caractère :\n");
+    Player1.name.namec3 = getchar();
+    getchar();
+    printf("\n");
+
+    printf("Pour le moment votre pseudos se constitue de %c%c%c, Voulez vous continué ?\n\n1.Oui\n2.Non\n", Player1.name.namec1, Player1.name.namec2, Player1.name.namec3);
+    scanf("%d",&sure);
+    printf("\n");
+
+    if (sure == yes)
+    {
+        printf("Ecrivez le quatrième caractère :\n");
+        getchar();
+        Player1.name.namec4 = getchar();
+        printf("\n");
+        printf("Maintenant votre pseudo se constitue de %c%c%c%c, Voulez vous continué ?\n\n1.Oui\n2.Non\n\n", Player1.name.namec1, Player1.name.namec2, Player1.name.namec3, Player1.name.namec4);
+        scanf("%d",&sure);
+        printf("\n");
+
+        if (sure == yes) 
+        {
+            printf("Ecrivez le cinquième caractère :\n");
+            getchar();
+            Player1.name.namec5 = getchar();
+            printf("\n");
+            printf("Maintenant votre pseudo se constitue de %c%c%c%c%c, Voulez vous continué ?\n\n1.Oui\n2.Non\n\n", Player1.name.namec1, Player1.name.namec2, Player1.name.namec3, Player1.name.namec4, Player1.name.namec5);
+            scanf("%d",&sure);
+            printf("\n");
+
+            if (sure == yes) 
+            {
+                printf("Ecrivez le sixième caractère :\n");
+                getchar();
+                Player1.name.namec6 = getchar();
+                printf("\n");
+                printf("Maintenant votre pseudo se constitue de %c%c%c%c%c%c, Voulez vous continué ?\n\n1.Oui\n2.Non\n\n", Player1.name.namec1, Player1.name.namec2, Player1.name.namec3, Player1.name.namec4, Player1.name.namec5, Player1.name.namec6);
+                scanf("%d",&sure);
+                printf("\n");
+
+                if (sure == yes) 
+                {
+                    printf("Ecrivez le septième caractère :\n");
+                    getchar();
+                    Player1.name.namec7 = getchar();
+                    printf("\n");
+                    printf("Maintenant votre pseudo se constitue de %c%c%c%c%c%c%c, Voulez vous continué ?\n\n1.Oui\n2.Non\n\n", Player1.name.namec1, Player1.name.namec2, Player1.name.namec3, Player1.name.namec4, Player1.name.namec5, Player1.name.namec6, Player1.name.namec7);
+                    scanf("%d",&sure);
+                    printf("\n");    
+
+                    if (sure == yes) 
+                    {
+                        printf("Ecrivez le huitième et dernier caractère :\n");
+                        getchar();
+                        Player1.name.namec8 = getchar();
+                        printf("\n");
+                        PlayerName();
+                        printf("\n");
+                    }    
+                }
+            }            
+        }                
+    }
+    
+    PlayerName();
+    printf("\n");
 
     // if role nanan attaque et defe == x tout les role et finir avec un else ?
-    
 
-    printf("Vous êtes envoyé au Japon pour combattre Pépé Turbo qui veut éliminer Mémé Turbo, seul problème si Pépé Turbo élimine Mémé Turbo elle ne nous révélera jamais ou est cacher la deuxième boule d'Okarun.\n");
-    printf("Choisissez une classe pour anéantir Pépé Turbo :\n- Ghost\n- Alien\n- Possessed\n- Medium\n");
+    printf("Vous êtes envoyé au Japon pour combattre Pépé Turbo qui veut éliminer Mémé Turbo, seul problème si Pépé Turbo élimine Mémé Turbo elle ne nous révélera jamais ou est cacher la deuxième boule d'Okarun.\n\n");
+    printf("Choisissez une classe pour anéantir Pépé Turbo :\n\n- Ghost\n- Alien\n- Possessed\n- Medium\n");
     
     
     
