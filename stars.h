8dc58740005f1e4b0f240963a1215a3d6f0a48d9
WireLess-00 <82403611+WireLess-00@users.noreply.github.com>
2026-09-08 16:57:25 +0200

    Update and rename subtom.h to stars.h

--- stars.h
+++ stars.h
@@ -1,16 +1,16 @@
 /*#include <stdio.h>*/
 
-#ifndef __SUBTOM__ /*Pour éviter d'inclure un fichier plusieurs fois à la compilation par erreur, temps de compilation plus rapide*/
-#define __SUBTOM__ /*Gardiens = Protection si Subtom pas définit, il le définit*/
+#ifndef __STARS__ /*Pour éviter d'inclure un fichier plusieurs fois à la compilation par erreur, temps de compilation plus rapide*/
+#define __STARS__ /*Gardiens = Protection si stars pas définit, il le définit*/
 
-void submtom();
+void stars();
 
 static int var = 7;/*Variables globales, ne peut être que static ici car sinon il pense qu'il doit diviser la variable en deux et quelle est donc différentes dans les deux fichier*/
 
 /*On peut aussi y mettre unions, énum, struc...*/
 
 /*{
-    printf("Je suis Tom et un esclave !\n");
+    printf("Je suis une étoiles !\n");
 }
 
 En général dans le .h il n'y a que le prototype de la fonction puis le code dans le .c qui porte le meme nom*/
