7b6d7e0363926598e9c3678e2c098336e802afcc
WireLess-00 <82403611+WireLess-00@users.noreply.github.com>
2026-09-08 16:57:36 +0200

    Update and rename subtom.c to stars.c

---
+++ stars.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "stars.h" /*Pour faire le lien avec le prototype dans le .h*/
+
+void stars()
+{
+    printf("Je suis une étoile !\n");
+}
+/*Code de ma fonction*/
--- subtom.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "subtom.h" /*Pour faire le lien avec le prototype dans le .h*/
-
-void submtom()
-{
-    printf("Je suis Tom et un esclave !\n");
-}
-/*Code de ma fonction*/
